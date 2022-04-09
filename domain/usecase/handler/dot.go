456728fe924ca6e9f06ede8d06f9dd20be979e71
artworkk <prem@cleverse.com>
2022-04-10 04:32:15 +0700

    feat: add goroutines in dot handler func

--- domain/usecase/handler/dot.go
+++ domain/usecase/handler/dot.go
@@ -17,31 +17,35 @@ func (h *handler) HandleWithDoT(w dns.ResponseWriter, r *dns.Msg) {
 	var cacheMissed bool
 	var m = make(answerMap)
 	var wg sync.WaitGroup
-	for _, q := range r.Question {
-		tString := dnsutils.DnsTypes[q.Qtype]
-		k := dnsutils.NewKey(q.Name, q.Qtype)
-		cached, found := h.repository.Get(k)
-		if found {
-			log.Printf("cache hit for \"%s\" [%s]", k.String(), tString)
-			msg.Answer = append(msg.Answer, cached...)
-		} else {
-			log.Printf("cache missed for \"%s\" [%s]", k.String(), tString)
-			cacheMissed = true
-			ans, rtt, err := h.dotClient.Exchange(r)
-			if err != nil {
-				log.Printf("failed to connect upstream: %v\n", err.Error())
-				return
+	for _, question := range r.Question {
+		wg.Add(1)
+		go func(q dns.Question) {
+			defer wg.Done()
+			tString := dnsutils.DnsTypes[q.Qtype]
+			k := dnsutils.NewKey(q.Name, q.Qtype)
+			cached, found := h.repository.Get(k)
+			if found {
+				log.Printf("cache hit for \"%s\" [%s]", k.String(), tString)
+				msg.Answer = append(msg.Answer, cached...)
+			} else {
+				log.Printf("cache missed for \"%s\" [%s]", k.String(), tString)
+				cacheMissed = true
+				ans, rtt, err := h.dotClient.Exchange(r)
+				if err != nil {
+					log.Printf("failed to connect upstream: %v\n", err.Error())
+					return
+				}
+				log.Printf("question: %s rtt: %v", q.String(), rtt)
+				if ans.Answer != nil {
+					msg.Answer = append(msg.Answer, ans.Answer...)
+					m[k] = append(m[k], ans.Answer...)
+				}
 			}
-			log.Printf("question: %s rtt: %v", q.String(), rtt)
-			if ans.Answer != nil {
-				msg.Answer = append(msg.Answer, ans.Answer...)
-				m[k] = append(m[k], ans.Answer...)
-			}
-		}
+		}(question)
 	}
+	wg.Wait()
 	w.WriteMsg(msg)
 	if cacheMissed && len(m) > 0 {
 		h.repository.SetMap(m)
 	}
-	wg.Wait()
 }
